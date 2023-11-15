f157f44c6605778bd8662636e79745ca82291f1a
Priya Jain <priya2030.info@gmail.com>
2023-11-15 19:36:15 +0530

    Basic server create form setup

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import { InitialModel } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import initialProfile from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -18,7 +19,11 @@ const SetupPage = async () => {
     return redirect(`/servers/${server.id}`);
   }
 
-  return <div>Create a Server</div>;
+  return (
+    <div>
+      <InitialModel />
+    </div>
+  );
 };
 
 export default SetupPage;
