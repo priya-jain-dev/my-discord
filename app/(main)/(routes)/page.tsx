93fb0361ab8b6a79d05989087276a007cddc5a96
Priya Jain <priya2030.info@gmail.com>
2023-11-15 17:50:41 +0530

    theme setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
@@ -10,6 +11,7 @@ export default function Home() {
           afterSwitchSessionUrl="/"
           afterMultiSessionSingleSignOutUrl="/"
         />
+        <ModeToggle />
       </p>
     </div>
   );
