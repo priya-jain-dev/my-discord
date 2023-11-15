cfbf8a731ee00c2e0b391b52c0f7ab9e9c6ddb0b
Priya Jain <priya2030.info@gmail.com>
2023-11-15 17:43:40 +0530

    fix redirect url part

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <p>Protected routes</p>
+      <p>
+        <UserButton
+          afterSignOutUrl="/"
+          afterSwitchSessionUrl="/"
+          afterMultiSessionSingleSignOutUrl="/"
+        />
+      </p>
     </div>
   );
 }
